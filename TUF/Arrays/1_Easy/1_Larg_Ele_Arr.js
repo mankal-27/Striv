6bf56484adb6cea9be6b46d1a1b21426ae7fb225
mankal-27 <mkalkutagi27@gmail.com>
2025-09-18 12:06:12 +0530

    checking logs

--- TUF/Arrays/1_Easy/1_Larg_Ele_Arr.js
+++ TUF/Arrays/1_Easy/1_Larg_Ele_Arr.js
@@ -7,5 +7,6 @@ function FindLargestElement(arr) {
     }
     return max
 }
+console.log("We are checking this₹")
 
 console.log(FindLargestElement([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 4, 92]))
